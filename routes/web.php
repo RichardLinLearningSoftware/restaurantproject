32ace785b2ceeba4f2093de6a0380790ca462ca5
Dumbdipshit <1211675@student.roc-nijmegen.nl>
2026-04-10 09:31:23 +0200

    Login/Register commit
    -Added a missing file for the authentication pages to work.

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@ Route::get('/menu', [MenuController::class, 'index'])->name('menu');
 
 Route::get('/createItem', [MenuController::class, 'create'])->name('createItem');
 Route::post('/menus', [MenuController::class, 'store']);
+
+require __DIR__.'/auth.php';
