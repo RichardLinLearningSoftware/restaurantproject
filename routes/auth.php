f02c1a3de932f24b286a514d59b11493e326917f
Dumbdipshit <1211675@student.roc-nijmegen.nl>
2026-04-13 10:38:54 +0200

    Prevent login and register if already logged in
    -The user can no longer acess the login/register page if they are loggedin

--- routes/auth.php
+++ routes/auth.php
@@ -4,16 +4,20 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
-// Register
-Route::get('/register', [RegisteredUserController::class, 'create'])
-    ->name('register');
+// Allows the user to acces the register and login page if they arent loggedin
+Route::middleware('guest')->group(function () {
+    //Register
+    Route::get('/register', [RegisteredUserController::class, 'create'])
+        ->name('register');
+    //Login
+    Route::get('/login', [AuthenticatedSessionController::class, 'create'])
+    ->name('login');
+});
 
+// Receive post request from /register
 Route::post('/register', [RegisteredUserController::class, 'store']);
 
-// Login
-Route::get('/login', [AuthenticatedSessionController::class, 'create'])
-    ->name('login');
-
+// Receive post request from /login
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 
 // Logout
